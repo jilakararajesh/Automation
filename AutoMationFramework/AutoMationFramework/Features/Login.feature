aa09f50a9460fdcddf16efb18dfd1dcf5f322edb
Rajesh <jilakararajesh@gmail.com>
2016-08-30 22:24:37 +0530

    Initial commit.

---
+++ AutoMationFramework/AutoMationFramework/Features/Login.feature
@@ -0,0 +1,10 @@
+﻿Feature: Login
+
+@Login
+Scenario: Login Verification
+Given I Launch The Browser
+When I navigate to "https://mozart-uat.iso.com"
+Then The Login Should Display
+When User Log in With "AMcustomeradmin" and "Verisk@123"
+Then Login should be successfull
+	
---
+++ AutoMationFramework/AutoMationFramework/StepDefinitons/Login Stepdef.cs
@@ -0,0 +1,55 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AutoMationFramework.StepDefinitons
+{
+    [Binding]
+    class first
+    {
+        IWebDriver driver;
+        [Given(@"I Launch The Browser")]
+        public void GivenILaunchTheBrowser()
+        {
+
+            driver = new FirefoxDriver();
+            driver.Manage().Window.Maximize();
+        }
+
+        [When(@"I navigate to ""(.*)""")]
+        public void WhenINavigateTo(string p0)
+        {
+            //ScenarioContext.Current.Pending();
+            driver.Navigate().GoToUrl("https://mozart-uat.iso.com");
+        }
+        [Then(@"The Login Should Display")]
+        public void ThenTheLoginShouldDisplay()
+        {
+            //ScenarioContext.Current.Pending();
+            Assert.IsNotNull(driver.FindElement(By.Id("signIn")));
+        }
+        [When(@"User Log in With ""(.*)"" and ""(.*)""")]
+        [When(@"User Log in With ""(.*)"" and ""(.*)""")]
+        public void WhenUserLogInWithAnd(string p0, string p1)
+        {
+            driver.FindElement(By.Id("username")).SendKeys("p0");
+            driver.FindElement(By.Id("password")).SendKeys("p1");
+
+        }
+        
+        [Then(@"Login should be successfull")]
+        public void ThenLoginShouldBeSuccessfull()
+        {
+            Assert.IsNotNull(driver.FindElement(By.ClassName("ng-binding")));
+        }
+
+
+    }
+
+}
