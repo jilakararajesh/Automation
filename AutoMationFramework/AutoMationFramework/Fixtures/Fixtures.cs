2913311e65266c2c2cbe0c1d8a4b08ccd7dd1b21
Rajesh <jilakararajesh@gmail.com>
2016-08-30 22:31:13 +0530

    Added files

---
+++ AutoMationFramework/AutoMationFramework/Fixtures/Fixtures.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoMationFramework.Fixtures
+{
+    class Fixtures
+    {
+
+        IWebDriver driver;
+            public void diver()
+        {
+            driver.Manage().Window.Maximize();
+        }
+        
+        
+        
+        
+    }
+}
